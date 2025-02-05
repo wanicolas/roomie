ffa4bac5b420e20e1f935d7169af2b9b99fdb43c
arena <nicolasarena.nico@gmail.com>
2025-02-05 14:51:46 +0100

    modification rooms

--- Roomie.Backend/Migrations/20250205134644_Rooms.Designer.cs
+++ Roomie.Backend/Migrations/20250205134644_Rooms.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Roomie.Backend.Data;
 
@@ -10,9 +11,11 @@ using Roomie.Backend.Data;
 namespace Roomie.Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250205134644_Rooms")]
+    partial class Rooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
--- Roomie.Backend/Models/Rooms.cs
+++ Roomie.Backend/Models/Rooms.cs
@@ -1,11 +1,18 @@
 namespace Roomie.Backend.Models
 {
-    public class Room
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Capacity { get; set; }
-        public bool AccessiblePMR { get; set; }
-        public List<string> Equipments { get; set; } = new();
-    }
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Building { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+    public DateOnly AvailableDate { get; set; }
+    public TimeOnly AvailableStartTime { get; set; }
+    public TimeOnly AvailableEndTime { get; set; }
+    public double Surface { get; set; }
+    public bool AccessiblePMR { get; set; }
+    public List<string> Equipments { get; set; } = new();
+    public int MinSeatingCapacity { get; set; }
+    public string? ImageUrl { get; set; }  // Nouvelle propriété
+}
 }
