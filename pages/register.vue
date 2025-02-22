4b737675d9d7a527651ca7b8095a3142ad6bd5f2
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-02-22 23:06:05 +0100

    feat(register): add new page

---
+++ pages/register.vue
@@ -0,0 +1,27 @@
+<template>
+	<h1 class="mb-6 text-center text-2xl font-semibold md:mb-12 md:text-4xl">
+		Inscription
+	</h1>
+	<div class="m-3 mb-12 sm:mx-auto sm:max-w-lg">
+		<UForm class="mb-6">
+			<UFormGroup label="Entrez votre email" class="mb-3">
+				<UInput icon="ph:envelope" />
+			</UFormGroup>
+			<UFormGroup label="Créez un mot de passe" class="mb-6">
+				<UInput icon="ph:lock" />
+			</UFormGroup>
+
+			<UButton type="submit" block>Créer mon compte</UButton>
+		</UForm>
+
+		<p class="text-center">
+			Déjà membre ?
+			<NuxtLink
+				to="/login"
+				class="text-primary-700 underline-offset-4 hover:underline dark:text-primary-300"
+			>
+				On se connecte !
+			</NuxtLink>
+		</p>
+	</div>
+</template>
