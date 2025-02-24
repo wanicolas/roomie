d73162882d5d0e0824140ac74111ea5f3ce591da
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-02-24 09:37:27 +0100

    feat(login): better form submit

--- pages/login.vue
+++ pages/login.vue
@@ -3,71 +3,60 @@
 		Connexion
 	</h1>
 	<div class="m-3 mb-12 sm:mx-auto sm:max-w-lg">
-		<UForm @submit.prevent="login(email, password)" class="flex flex-col gap-3">
-			<UFormGroup label="Email">
-				<UInput v-model="email" icon="ph:envelope" type="email" required />
+		<UForm @submit.prevent="login" class="mb-6">
+			<UFormGroup label="Email" class="mb-3">
+				<UInput v-model="email" required icon="ph:envelope" type="email" />
 			</UFormGroup>
-			<UFormGroup label="Mot de passe">
-				<UInput v-model="password" icon="ph:lock" type="password" required />
+			<UFormGroup label="Mot de passe" class="mb-6">
+				<UInput v-model="password" required icon="ph:lock" type="password" />
 			</UFormGroup>
 
 			<UButton type="submit" block>Se connecter</UButton>
-
-			<p v-if="errorMessage" class="text-red-500 mt-2">{{ errorMessage }}</p>
 		</UForm>
-		<!-- ✅ Ajout du bouton d'inscription -->
-		<div class="mt-4 text-center">
-			<p class="text-gray-600">Vous n'avez pas de compte ?</p>
-			<UButton color="gray" variant="link" @click="router.push('/register')">
-				Créer un compte
-			</UButton>
-		</div>
+
+		<p class="text-center">
+			Pas encore de compte ?
+			<NuxtLink
+				to="/register"
+				class="text-primary-700 underline-offset-4 hover:underline dark:text-primary-300"
+			>
+				On va régler ça !
+			</NuxtLink>
+		</p>
 	</div>
+
+	<p v-if="error" class="text-red-600 dark:text-red-400">{{ error }}</p>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+useHead({
+	title: "Connexion - Roomie, gestion et réservation de salles",
+	meta: [
+		{
+			name: "description",
+			content: "Connexion à votre compte Roomie.",
+		},
+	],
+});
 
-const email = ref('')
-const password = ref('')
-const errorMessage = ref(null)
-const router = useRouter()
-const token = useState('token', () => null)
+const email = ref("");
+const password = ref("");
 
 const login = async () => {
-  errorMessage.value = "";
+	const {
+		data: auth_token,
+		error,
+		status,
+	} = await $fetch("http://localhost:5184/api/Auth/login", {
+		body: JSON.stringify({
+			email: email.value,
+			password: password.value,
+		}),
+	});
 
-  try {
-    const response = await fetch("http://localhost:5184/api/Auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: email.value,
-        password: password.value,
-      }),
-    });
-
-    if (!response.ok) {
-      let errorData;
-      try {
-        errorData = await response.json();
-      } catch {
-        throw new Error("Erreur serveur, réponse invalide.");
-      }
-      console.error("Erreur API :", errorData);
-      throw new Error(errorData.message || "Une erreur est survenue.");
-    }
-
-    const data = await response.json();
-    console.log("Réponse API :", data);
-
-    localStorage.setItem("token", data.token);
-    token.value = data.token;
-    router.push("/");
-  } catch (error) {
-    console.error("Erreur de connexion :", error.message);
-    errorMessage.value = error.message;
-  }
+	if (!error) {
+		useCookie("auth_token", auth_token);
+		router.push("/book");
+	}
 };
 </script>
