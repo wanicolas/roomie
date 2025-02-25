af8a20e93fcbd89bb360502bdbb6b155baf0dfbd
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-02-25 01:03:00 +0100

    feat(profile): add booking historic

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -6,9 +6,25 @@
 		<UButton @click="logout">Déconnexion</UButton>
 
 		<div v-for="info in userInfos">
-			<p>Nom : {{ info.name }}</p>
-			<p>Rôle : {{ info.role }}</p>
+			<p>Email : {{ info.email }}</p>
+			<p>Nom : {{ info.fullName }}</p>
+			<p>Rôle(s) : {{ info.roles }}</p>
 		</div>
+	</div>
+
+	<h2 class="m-3 mb-6 max-w-screen-xl text-2xl font-semibold xl:mx-auto">
+		Historique des réservations
+	</h2>
+	<div v-for="booking in userInfos.bookings">
+		<p>
+			Salle :
+			<NuxtLink :to="`/room/${booking.roomId}`">
+				{{ booking.roomName }}
+			</NuxtLink>
+		</p>
+		<p>Date : {{ booking.date }}</p>
+		<p>heure de début : {{ booking.startHour }}</p>
+		<p>heure de fin : {{ booking.endHour }}</p>
 	</div>
 </template>
 
@@ -23,4 +39,14 @@ const logout = () => {
 	useCookie("auth_token").value = "";
 	router.push("/");
 };
+
+const {
+	data: userInfos,
+	error,
+	status,
+} = await useFetch("http://localhost:5184/api/User/info", {
+	headers: {
+		Authorization: `Bearer ${useCookie("auth_token").value}`,
+	},
+});
 </script>
